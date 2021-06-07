27eb01e3d3bbe57a6e751593110bf7d6c603af0e
fanny <fanny.hunin.an123@gmail.com>
2021-06-07 10:25:42 +0200

    markdown working

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
+import { Component } from 'react';
 import './App.css';
+import marked from 'marked'
 
-function App() {
-  return (
-    <div>
+import{sampleText} from './sampleText'
+
+class App extends Component {
+  state = {
+    text : sampleText
+  }
+
+  handleChange = (event) => {
+    const textCopy = {... this.state.text}
+    const text = event.target.value
+    this.setState({ text })
+  }
+
+  renderText = (text) => {
+    //sanitize fait partie de la bibliothèque marked, il supprime le rendu en balises des élements html dans le markdown editor
+    const __html = marked(text, {sanitize:true});
+    return {__html}
+  }
+
+  render(){
+    return (
       <div className="container">
         <div className="row">
           <div className="col-sm-6">
-            <textarea className="form-control" rows="35"></textarea>
+            <textarea className="form-control" rows="35" value={this.state.text} onChange={this.handleChange}></textarea>
           </div>
-          <div className="col-sm-6">
-            <h1>Résultat</h1>
+          <div className='col-sm-6'>
+            <div dangerouslySetInnerHTML={this.renderText(this.state.text)}></div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
